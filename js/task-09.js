dd57106db3aab8a908876e80eb6fdda6d7c19272
KulinichII <126084720+KulinichII@users.noreply.github.com>
2023-06-14 16:58:54 +0300

    пщ

---
+++ js/task-09.js
@@ -0,0 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const widget = document.querySelector('.widget');
+const colorSpan = widget.querySelector('.color');
+const changeColorBtn = widget.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', handleChangeColor);
+
+function handleChangeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
